010e78b8692029a0442f058bf2ba97962e4470c0
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-06-22 10:21:36 +0100

    feat: ensure generatePomFileForShadowPublication runs after clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,6 +53,9 @@ afterEvaluate {
     }
 }
 
+tasks.named("generatePomFileForShadowPublication") {
+    mustRunAfter(tasks.named("clean"))
+}
 tasks.register("publishToAuxilor") {
     dependsOn(tasks.named("publishShadowPublicationToAuxilorRepository"))
 }
